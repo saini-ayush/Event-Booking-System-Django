962d19d16b0633878f71ccda90f563c5a4c3f8df
Ayush Saini <ayush.saini@consultadd.com>
2025-02-24 18:02:40 +0530

    Updated README

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lhl$(%f!z4)#z8)-3cmp^37*32*^b)f!660%ez!j73f9@2bb6#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
